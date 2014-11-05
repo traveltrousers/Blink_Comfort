0dd3a77d7c2678a1a3ac42916352c6ada5ac2590
root <root@tt.betterboyfriendbuilder.com>
2014-11-05 19:55:43 +0000

    initial commit

---
+++ Assets/bottom_script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bottom_script : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	void Update()
+	{
+		//Use keys to ratchet rotation
+		if (Input.GetKeyDown(KeyCode.Q))
+			animation.Play("bottom_lid");
+		if (Input.GetKeyDown(KeyCode.E))
+			animation.Play("bottom_lid");
+	}
+}
---
+++ Assets/top_script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class top_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+void Update()
+{
+	//Use keys to ratchet rotation
+	if (Input.GetKeyDown(KeyCode.Q))
+		animation.Play("top_lid");
+
+	if (Input.GetKeyDown(KeyCode.E))
+		animation.Play("top_lid");
+}
+}
