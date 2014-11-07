105be6477ae1099c34d53b08599027fc33c27b8d
traveltrousers <github@traveltrousers.com>
2014-11-07 22:05:21 +0000

    flashh

---
+++ Assets/flash.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+	
+		function Start () {
+		renderer.enabled = false;
+	}
+
+	var Blinker : int = 0;
+	var Freq : int = 19;
+	
+
+		function  Update () {
+		
+		
+				if (Input.GetKeyDown(KeyCode.Z))
+			Freq++;
+			
+					if (Input.GetKeyDown(KeyCode.X))
+			Freq--;
+		
+
+		if (Blinker == 0)
+	{
+	renderer.enabled = false;
+	}
+
+
+	Blinker++;
+	if (Blinker > Freq)
+	{
+	Blinker = 0;
+	renderer.enabled = true;
+	}
+
+
+
+}
